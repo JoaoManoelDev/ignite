bda7c195fc939562526b1bfe3c42fc4149d222fb
João Manoel <joaomanoeldev@gmail.com>
2024-04-08 16:10:25 -0300

    node: creation and listing routes (HTTP methods)

--- nodejs/01-fundamentals-nodejs/src/server.js
+++ nodejs/01-fundamentals-nodejs/src/server.js
@@ -1,6 +1,20 @@
 import http from "node:http"
 
 const server = http.createServer((request, response) => {
+  const { method, url } = request
+  
+  console.log("METHOD", method)
+  console.log("URL", url)
+
+
+  if (method === "GET" && url === "/users") {
+    return response.end("List of users")
+  }
+
+  if (method === "POST" && url === "/users") {
+    return response.end("Creating users")
+  }
+
   return response.end("Hello World")
 })
 
