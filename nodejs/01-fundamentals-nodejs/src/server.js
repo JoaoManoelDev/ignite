269cccf7f2f8d650b18394eb4088394b01f3b4f1
João Manoel <joaomanoeldev@gmail.com>
2024-04-08 16:35:10 -0300

    node: saving users in memory (headers)

--- nodejs/01-fundamentals-nodejs/src/server.js
+++ nodejs/01-fundamentals-nodejs/src/server.js
@@ -1,17 +1,38 @@
 import http from "node:http"
 
+/*
+  Stateful => Sempre tem informação guardada em memória (Depende das informações
+    que são salvas em memória para que ela funcione. Ao perder esses dados em
+    memória ela pode funcionar diferente.)
+
+  Stateless => Não salva nada em memória. Salva informações em dispositivos
+    externos como banco de dados, arquivos de texto etc. Ao fechar o sistema e
+    abrir ela funcionará igual.
+
+  Headers (request/response) => Metadados. Informações adicionais que não te a
+  ver com o dado retornado do back-end para o front-ent, mas sim de como que
+  aquele dado pode ser interpretado pelo front-end.
+*/
+
+// APP STATEFUL
+const users = []
+
 const server = http.createServer((request, response) => {
   const { method, url } = request
   
-  console.log("METHOD", method)
-  console.log("URL", url)
-
-
   if (method === "GET" && url === "/users") {
-    return response.end("List of users")
+    return response
+      .setHeader("Content-type", "application/json")
+      .end(JSON.stringify(users))
   }
 
   if (method === "POST" && url === "/users") {
+    users.push({
+      id: 1,
+      name: "John Doe",
+      email: "johndoe@example.com"
+    })
+
     return response.end("Creating users")
   }
 
