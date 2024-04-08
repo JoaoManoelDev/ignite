3c050f655faa0d4ca85dea6bb85239f84bfade96
João Manoel <joaomanoeldev@gmail.com>
2024-04-08 20:07:11 -0300

    node: applying streams to the HTTP module

---
+++ nodejs/01-fundamentals-nodejs/streams/stream-http-server.js
@@ -0,0 +1,23 @@
+import http from "node:http"
+import { Transform } from "node:stream"
+
+class InverseNumberStream extends Transform {
+  _transform(chunk, encoding, callback) {
+    const transformed = Number(chunk.toString()) * -1
+
+    console.log(transformed)
+
+    callback(null, Buffer.from(String(transformed)))
+  }
+}
+
+// request  => ReadableStream
+// response => WritableStream
+
+const server = http.createServer((request, response) => {
+  return request
+    .pipe(new InverseNumberStream())
+    .pipe(response)
+})
+
+server.listen(3334)
