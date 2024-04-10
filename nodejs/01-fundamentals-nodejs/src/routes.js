7dc2d9d50fba2747184bd26c98a17727180dd011
João Manoel <joaomanoeldev@gmail.com>
2024-04-10 12:32:39 -0300

    filtering the database list

--- nodejs/01-fundamentals-nodejs/src/routes.js
+++ nodejs/01-fundamentals-nodejs/src/routes.js
@@ -10,8 +10,12 @@ export const routes = [
     method: "GET",
     path: buildRoutePath("/users"),
     handler: (request, response) => {
-      console.log(request.query)
-      const users = database.select("users")
+      const { search } = request.query
+
+      const users = database.select("users", search ? {
+        name: search,
+        email: search
+      } : null)
 
       return response.end(JSON.stringify(users))
     }
