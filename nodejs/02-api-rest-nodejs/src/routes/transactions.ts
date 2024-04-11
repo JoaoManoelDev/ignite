f9246a06d5396f098c0d9b834118110ded7fdca5
João Manoel <joaomanoeldev@gmail.com>
2024-04-11 14:19:05 -0300

    refactor: create plugin to transactions routes and move to routes

---
+++ nodejs/02-api-rest-nodejs/src/routes/transactions.ts
@@ -0,0 +1,11 @@
+import { FastifyInstance } from "fastify"
+
+import { knex } from "../database"
+
+export const transactionsRoutes = async (app: FastifyInstance) => {
+  app.get("/hello", async () => {
+    const tables = await knex("sqlite_schema").select("*")
+    
+    return tables
+  })
+}
--- nodejs/02-api-rest-nodejs/src/server.ts
+++ nodejs/02-api-rest-nodejs/src/server.ts
@@ -1,18 +1,14 @@
 import fastify from "fastify"
 
-import { knex } from "./database"
 import { env } from "./env"
+import { transactionsRoutes } from "./routes/transactions"
 
 const app = fastify()
 
-app.get("/hello", async () => {
-  const tables = await knex("sqlite_schema").select("*")
-
-  return tables
-})
+app.register(transactionsRoutes)
 
 app.listen({
   port: env.PORT
 }).then(() => {
-  console.log("HTTP Server Running")
+  console.log(`HTTP Server Running in por ${env.PORT}`)
 })
