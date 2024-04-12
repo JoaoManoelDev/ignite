7a05b21965012857529aa1568f81f4c865cdd89c
João Manoel <joaomanoeldev@gmail.com>
2024-04-12 14:03:23 -0300

    feat: create route to list specific transaction

--- nodejs/02-api-rest-nodejs/src/routes/transactions.ts
+++ nodejs/02-api-rest-nodejs/src/routes/transactions.ts
@@ -11,6 +11,18 @@ export const transactionsRoutes = async (app: FastifyInstance) => {
     return { transactions }
   })
 
+  app.get("/:id", async (request) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid()
+    })
+    
+    const { id } = getTransactionParamsSchema.parse(request.params)
+
+    const transaction = await knex("transactions").where('id', id).first()
+
+    return { transaction }
+  })
+
   app.post("/", async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
