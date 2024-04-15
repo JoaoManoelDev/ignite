4e8800d1e19a4d8bfe42aaf9b0444c1b89ed3637
João Manoel <joaomanoeldev@gmail.com>
2024-04-15 16:52:27 -0300

    test: create execution migrations before each test

--- nodejs/02-api-rest-nodejs/test/transactions.spec.ts
+++ nodejs/02-api-rest-nodejs/test/transactions.spec.ts
@@ -1,4 +1,5 @@
-import { afterAll, beforeAll, describe, expect, it } from "vitest"
+import { execSync } from "node:child_process"
+import { afterAll, beforeAll, beforeEach, describe, expect, it } from "vitest"
 import request from "supertest"
 
 import { app } from "../src/app"
@@ -10,6 +11,11 @@ describe("Transactions routes", () => {
   
   afterAll(async () => {
     await app.close()
+  })
+
+  beforeEach(() => {
+    execSync("npm run knex migrate:rollback --all")
+    execSync("npm run knex migrate:latest")
   })
   
   it("Should be able a user create a new transaction", async () => {
