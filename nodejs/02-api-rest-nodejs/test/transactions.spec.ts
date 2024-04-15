cfec40dd19996aab832dc78f8ef87908024fb1dc
João Manoel <joaomanoeldev@gmail.com>
2024-04-15 14:54:26 -0300

    test: categorize transactions tests

--- nodejs/02-api-rest-nodejs/test/transactions.spec.ts
+++ nodejs/02-api-rest-nodejs/test/transactions.spec.ts
@@ -1,24 +1,26 @@
-import { afterAll, beforeAll, expect, test } from "vitest"
+import { afterAll, beforeAll, describe, expect, it } from "vitest"
 import request from "supertest"
 
 import { app } from "../src/app"
 
-beforeAll(async () => {
-  await app.ready()
-})
-
-afterAll(async () => {
-  await app.close()
-})
-
-test("Should be able a user register a new transaction", async () => {
-  const response = await request(app.server)
-    .post("/transactions")
-    .send({
-      title: "New transaction",
-      amount: 5000,
-      type: "credit"
-    })
+describe("Transactions routes", () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
   
-  expect(response.statusCode).toEqual(201)
+  afterAll(async () => {
+    await app.close()
+  })
+  
+  it("Should be able a user create a new transaction", async () => {
+    const response = await request(app.server)
+      .post("/transactions")
+      .send({
+        title: "New transaction",
+        amount: 5000,
+        type: "credit"
+      })
+    
+    expect(response.statusCode).toEqual(201)
+  })
 })
